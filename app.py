f053f88c4e2cdf15fbc763f94b3f21d4155fd97b
AnnaShajee <annaasha@gmail.com>
2018-04-08 02:30:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import urllib, json,os, sys
+import urllib, json, os, sys
 from flask import Flask, request, make_response
 import requests
 
@@ -9,25 +9,16 @@ def verify():
         #Webhook verification
         return "Project verified", 200
 
-headers = {
-        'Authorization': 'Bearer 28c2863125f64b9c8835c5d6c3b7aea1',
-        }
-response = requests.get('https://dialogflow.com/v1/query?v=20170712&e=Welcome&timezone=Asia/Calcutta&lang=en&sessionId=123456', headers=headers)
-
-params = (
-        ('v', '20170712'),
-        ('e', 'Welcome'),
-        ('lang', 'en'),
-        ('sessionId', 'fd11cd55-0dc0-4ba8-9404-a69f82a6e639'),
-        ('timezone', 'Asia/Calcutta'),
-        )
+@app.route('/webhook', methods=['GET'])
+def verify():
+        #Webhook verification
+        return "Webhook verified", 200
         
-@app.route('/', methods=['POST'])
-def webhook():
-        
+@app.route('/webhook', methods=['POST'])
+def webhook():        
         print("********")
         print("((((")
-        req = request.get_json()
+        req = request.get_json((silent=True, force=True))
         print("Request: ")
         print(json.dumps(req, indent = 4))
         res = makeWebhookResult(req)
@@ -59,5 +50,5 @@ def makeWebhookResult(req):
 if __name__ == '__main__':
         port = int(os.getenv('PORT', 80))
         print ("Starting on port %d" %(port))
-        app.run(debug = True, port = port)
+        app.run(debug = True, port = port, host='0.0.0.0')
 
