a11ea7e22c3323eb27d1d716159411128cc04e83
AnnaShajee <annaasha@gmail.com>
2018-04-11 09:25:56 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,8 +1,10 @@
 import utils
+import json
 
 def makeWebhookResult(req):
     action = req.get("result").get("action")
     if action == "findBranchLink":
+        print("A")
         result = findBranchLink(req)
         print(result)
     elif action == "findGuide":
