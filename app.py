b5b2a4f371aec5b9243890164da94dd449f77fae
AnnaShajee <annaasha@gmail.com>
2018-04-11 08:55:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
+import utils
+
 #Starting the app in a global context
 app = Flask(__name__)
 
@@ -35,114 +37,6 @@ def webhook():
     r = make_response(ret)
     r.headers['Content-Type'] = 'application/json'
     return r
-
-def makeWebhookResult(req):
-    action = req.get("result").get("action")
-    if action == "findBranchLink":
-        result = findBranchLink(req)
-        print(result)
-    elif action == "findGuide":
-        result = findGuide(req)
-    elif action == "findSyllabus":
-        result = findSyllabus(req)
-    elif action == "contactOffice":
-        result = contactOffice(req)
-    elif action == "admissionQuery":
-        result = admissionQuery(req)
-    else:
-        result = default()
-    print(result)
-    return result
-
-def findBranchLink(req):
-    result = req.get("result")
-    print (result)
-    parameters = result.get("parameters")
-    print(parameters)
-    branch = parameters.get("branch")
-    print(branch)
-    data = json.load(open('data.json'))
-    branches = data['branches']
-    print (branches)
-    flag = "false"
-    for index in range(len(branches)):
-        if branch == branches[index]['branch']:
-            flag = "true"
-            break
-    print (index)
-    print(flag)
-    if flag == "true":
-        link = data['branches'][index]['link']
-        name = data['branches'][index]['name']
-        school = data['branches'][index]['school']
-        speech1 = ("%s is available, under %s. " %(name, school))
-        speech2 = ("Read more at %s" % (link))
-        speech = speech1 + speech2
-        print (speech)
-        return {
-            "speech": speech,
-            "displayText": speech,
-            "source": "Institution-Chat-Bot",
-            "messages": [
-            {
-            "displayText": speech,
-            "platform": "google",
-            "textToSpeech": speech,
-            "type": "simple_response"
-            },
-            {
-            "buttons": [
-            {
-            "openUrlAction": {
-            "url": link
-            },
-            "title": "Read more here."
-            }
-            ],
-            "formattedText": speech1,
-            "platform": "google",
-            "subtitle": school,
-            "title": name,
-            "type": "basic_card"
-            }
-            ] 
-            }
-    else: 
-        speech1 = ("I'm sorry, we don't offer that course at VIT, Vellore. ")
-        link = "http://vit.ac.in/admissions/ug."
-        speech2 = ("Check out the courses offered at %s" %(link))
-        speech = speech1 + speech2
-        print(speech)
-        return {
-            "speech": speech,
-            "displayText": speech,
-            "source": "Institution-Chat-Bot",
-            "messages": [
-            {
-            "displayText": speech,
-            "platform": "google",
-            "textToSpeech": speech,
-            "type": "simple_response"
-            },
-            {
-            "buttons": [
-            {
-            "openUrlAction": {
-            "url": link
-            },
-            "title": "Find offered courses here."
-            }
-            ],
-            "formattedText": speech1,
-            "platform": "google",
-            "title": "Course not offered",
-            "type": "basic_card"
-            }
-            ] 
-            }
-
-
-
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 80))
